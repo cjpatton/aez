8c66171d8ccf74b3b96fbd6a69bf5aa33956bfc8
Christopher Patton <chrispatton@gmail.com>
2014-12-30 17:17:36 -0800

    Next, typemap vector of strings to char **.

--- python/aez.py
+++ python/aez.py
@@ -2,6 +2,29 @@ import _aez
 
 ABYTES = 16
 
+class AEZError(Exception): 
+  
+  def __init__(self, msg):
+    self.msg = msg
+
+  def __str__(self):
+    return 'AEZError:', self.msg
+
+def data2params(A): 
+  if len(A) == 0:
+    return (None, None, 0)
+  
+  elif len(A) > _aez.MAX_DATA:  
+    raise AEZError("Too many data (MAX_DATA=%d)" % _aez.MAX_DATA)
+  
+  else: 
+    tags = []; tag_bytes = []
+    for tag in A:
+      tags.append(str(tag))
+      tag_bytes.append(len(tags[-1]))
+    return (tags, tag_bytes, len(tags))
+
+
 class Context:
   
   def __init__(self, K):
@@ -10,61 +33,49 @@ class Context:
   def __del__(self):
     _aez.delete_Context(self.x)
 
-
-  def encipher(self, M, A=[]): 
+  def Encipher(self, M, A=[]): 
+    (tags, tag_bytes, num_tags) = data2params(A)
     C = '0' * len(M)
     _aez.aez_encipher(C, M, len(M),
-                      None, 0, None,
-                      #A, len(A), [len(a) for a in A],
+                      tags, num_tags, tag_bytes,
                       self.x, 0)
     return C
   
-  def decipher(self, C, A=[]): 
+  def Encipher(self, C, A=[]): 
+    (tags, tag_bytes, num_tags) = data2params(A)
     M = '0' * len(C)
     _aez.aez_encipher(M, C, len(C),
-                      None, 0, None,
-                      #A, len(A), [len(a) for a in A],
+                      tags, num_tags, tag_bytes,
                       self.x, 1)
     return M
 
-  def encrypt(self, M, N, A=[], abytes=ABYTES): 
+  def Encrypt(self, M, N, A=[], abytes=ABYTES): 
+    (tags, tag_bytes, num_tags) = data2params(A)
     C = '0' * (len(M) + abytes)
     _aez.aez_encrypt(C, M, len(M), N, len(N), 
-                     None, None, 0,
-                     #A, [len(a) for a in A], len(A), 
+                     tags, tag_bytes, num_tags,
                      abytes, self.x)
     return C
   
-  def decrypt(self, C, N, A=[], abytes=ABYTES): 
+  def Decrypt(self, C, N, A=[], abytes=ABYTES): 
+    (tags, tag_bytes, num_tags) = data2params(A)
     M = '0' * (len(C) - abytes)
     res = _aez.aez_decrypt(M, C, len(C), N, len(N), 
-                           None, None, 0,
-                           #A, [len(a) for a in A], len(A), 
+                           tags, tag_bytes, num_tags,
                            abytes, self.x)
     if res == _aez.INVALID:
       return None
     return M
 
-  def hash(self, A=[]):
+  def Hash(self, A=[]):
+    (tags, tag_bytes, num_tags) = data2params(A)
     H = '0' * 16
-    _aez.aez_hash(H, None, 0, None, 
-                  #A, len(A), [len(a) for a in A],
-                  self.x)
+    _aez.aez_hash(H, tags, num_tags, tag_bytes, self.x)
     return H
 
-  def prf(self, A=[], abytes=ABYTES):
+  def PRF(self, A=[], abytes=ABYTES):
+    (tags, tag_bytes, num_tags) = data2params(A)
     X = '0' * abytes
-    _aez.aez_prf(X, None, 0, None, 
-                 #A, len(A), [len(a) for a in A],
-                 abytes, self.x)
+    _aez.aez_prf(X, tags, num_tags, tag_bytes, abytes, self.x)
     return X
 
-
-def test():
-  
-  M = "This is an OK message."
-  N = "This is a great nonce."
-  context = Context('this is a great key')
-  C = context.encipher(M, N)
-  P = context.decipher(C, N)
-  print P 
